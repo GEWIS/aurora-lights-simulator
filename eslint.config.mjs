6f79d10cab7b3fed3aa2c68ef6b569b4cc8fdfd1
Roy Kakkenberg <me@roykakkenberg.nl>
2024-12-27 16:13:35 +0100

    feat: add Vite React template app

---
+++ eslint.config.mjs
@@ -0,0 +1,5 @@
+import eslint from '@gewis/eslint-config/eslint.common.mjs';
+import react from '@gewis/eslint-config/eslint.react.mjs';
+import prettier from '@gewis/eslint-config/eslint.prettier.mjs';
+
+export default [...eslint, ...react, ...prettier];
